e17be5e19d6a716323d86956158537b0e1efaa34
rivaldy mechdivicka <rmehdivika79@gmail.com>
2024-05-28 15:44:58 +0700

    Initial commit

---
+++ Backend CRUD/Backend-CRUD/src/main/java/com/example/Backend/CRUD/BackendCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backend.CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCrudApplication.class, args);
+	}
+
+}
